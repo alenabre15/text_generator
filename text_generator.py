42674562fce540eb50715fe648e249192f61b40e
alenabre15 <44479698+alenabre15@users.noreply.github.com>
2019-01-18 22:56:06 +0300

    Update text_generator.py

--- text_generator.py
+++ text_generator.py
@@ -1,10 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from keras_preprocessing import text
 
 from keras import load_model
